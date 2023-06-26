7ef4e424964c311623f6f6269bbdc8568da41a44
Rahul Baragur <iamrb202@gmail.com>
2023-06-26 11:10:11 +0530

    temp changes 3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
-import SocialLinks from "./components/SocialLinks";
+import SocialLinks from "./components/SocialsLinks";
 
 function App() {
   return (
