1f571f18c2112f353319e377f2b7a6c83dc6d698
Rahul Baragur <iamrb202@gmail.com>
2023-06-26 15:27:36 +0530

    v13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,11 @@ const About = () => {
   return (
   <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-900 via-gray-950 to-black text-[#1ED760]">
     <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-      <div className="pb-8">
-        <p className="text-4xl font-bold inline border-b-4 border-[#1ED760]  ">About</p>
+      <div className="pb-8 md:mt-40">
+        <p className="text-4xl font-bold inline border-b-4 border-[#1ED760]">About</p>
       </div>
 
-    <p className="text-xl mt-20 text-white">
+    <p className="text-xl text-white">
       Ever since I was a kid, I was always building and constructing, whether it were lego blocks, Minecraft structures, or even making forts out of pillows. The first time I was involved in the field of computer science was with Lego NXT, in which I programmed and created a robot of my bidding at 8 years old. The passion of creating soon translated into programming in which I got to exercise the same skills into making whatever I desired.
     </p>
 
