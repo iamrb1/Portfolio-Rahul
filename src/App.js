dd9831e60833147d5d4908b10bd8ae09cb7948e3
Rahul Baragur <iamrb202@gmail.com>
2023-06-27 16:36:05 +0530

    v35

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import Contacts from "./components/Place";
 import SocialsLinks from "./components/SocialsLinks";
-
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
       <Experience />
       <Contact />
       <Contacts />
+      <Footer />
 
       <SocialsLinks />
     </div>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+  <div name="footer" className="w-full h-10 bg-black">
+
+</div>
+)};
+
+export default Footer
