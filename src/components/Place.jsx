dd1aea26e9119599a38efdb15a3c5b8542cf91c2
Rahul Baragur <iamrb202@gmail.com>
2023-06-28 15:21:53 +0530

    revert

--- src/components/Place.jsx
+++ src/components/Place.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Place = () => {
   return (
-<div name='Place' className="landscape:hidden w-full h-80 bg-gradient-to-b from-gray-900 via-gray-950 to-black p-4 text-white">
+<div name='Place' className="portrait:hidden w-full h-80 bg-gradient-to-b from-gray-900 via-gray-950 to-black p-4 text-white">
 </div>
   
   );
